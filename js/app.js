3d78f4cad5fd09fc3fd2b8e44abe4ae1088dd15d
Aman Kumar <kumaraman140795@gmail.com>
2018-11-03 02:14:23 +0530

    Completed basic game logic

--- js/app.js
+++ js/app.js
@@ -42,10 +42,55 @@ const modal = {
   }
 };
 
+//////////////////////////////////////////////////////////////////////////////
+
 // Controller for the whole application...
 const controller = {
   // array to hold the index of currently open cards...
   openCards: [],
+
+  // handles the case of right selection...
+  openHandler: function openHandler() {
+    // adding .card-right to both cards...
+    controller.openCards[0].cardElement.classList.add("card-right");
+    controller.openCards[1].cardElement.classList.add("card-right");
+
+    setTimeout(function() {
+      // removing .card-right from both cards...
+      controller.openCards[0].cardElement.classList.remove("card-right");
+      controller.openCards[1].cardElement.classList.remove("card-right");
+
+      // flushing the controller.openCards...
+      controller.openCards = [];
+
+      // adding click listener to cards...
+      deckView.bindClickHandler();
+
+    }, 800);
+  },
+
+  // handles the case of error in selection...
+  closeHandler: function closeHandler() {
+    // adding .card-wrong to both cards...
+    controller.openCards[0].cardElement.classList.add("card-wrong");
+    controller.openCards[1].cardElement.classList.add("card-wrong");
+
+    setTimeout(function() {
+      // removing the class .card-wrong from both the cards...
+      controller.openCards[0].cardElement.classList.remove("card-wrong");
+      controller.openCards[1].cardElement.classList.remove("card-wrong");
+
+      // closing the cards as the cards do not match...
+      deckView.close(controller.openCards[0]);
+      deckView.close(controller.openCards[1]);
+
+      // flushing controller.openCards...
+      controller.openCards = [];
+
+      // adding click listener to cards...
+      deckView.bindClickHandler();
+    }, 800);
+  },
 
   init: function init() {
     // need to initialize the modal and shuffle the cards...
@@ -61,11 +106,13 @@ const controller = {
     const cardIndex = Number(cardElement.getAttribute("data-index"));
     const cardName = modal.returnCard(cardIndex);
 
-    // leaving this for now...
-    // returning the control if the card is already open...
-    // if(controller.openCards.length === 1){
-    //   return;
-    // }
+    // handle for case if the same card is clicked twice...
+    if (
+      controller.openCards.length == 1 &&
+      controller.openCards[0].cardElement === cardElement
+    ) {
+      return;
+    }
 
     // the card is not already open, so pushing it to openCards[] and opening it in the UI...
     controller.openCards.push({
@@ -74,62 +121,54 @@ const controller = {
     });
     deckView.open({ cardElement, cardName });
 
-    // if it is the second card that is opened, checking if the 2 cards match, and is so, leave the cards open...
+    // checking if it was the second selection...
     if (controller.openCards.length === 2) {
+
+      // removing the handler from deck
+      deckView.unbindClickHandler();
+
       // the 2 open cards are the same...
       if (
-        controller.openCards[0].cardName === controller.openCards[1].cardName
+        controller.openCards[0].cardName ===
+         controller.openCards[1].cardName
       ) {
-        // leaving the cards open as they matched...
-        // adding class .card-right to show that the selection was right...
-        controller.openCards[0].cardElement.classList.add("card-right");
-        controller.openCards[1].cardElement.classList.add("card-right");
-
-        setTimeout(function() {
-          // clearing the right selection after the timeout...
-          controller.openCards[0].cardElement.classList.remove("card-right");
-          controller.openCards[1].cardElement.classList.remove("card-right");
-
-          // flushing the controller.openCards...
-          controller.openCards = [];
-
-        }, 800);
+        controller.openHandler();
       }
 
       // 2 different cards are opened...
       else {
-        // adding class .card-wrong to show that the selection was wrong...
-        controller.openCards[0].cardElement.classList.add("card-wrong");
-        controller.openCards[1].cardElement.classList.add("card-wrong");
-
-        setTimeout(function() {
-
-          // removing the class .card-wrong from the cards...
-          controller.openCards[0].cardElement.classList.remove("card-wrong");
-          controller.openCards[1].cardElement.classList.remove("card-wrong");
-
-          // closing the cards as the cards do not match...
-          deckView.close(controller.openCards[0]);
-          deckView.close(controller.openCards[1]);
-
-          // flushing controller.openCards...
-          controller.openCards = [];
-        }, 1000);
+        controller.closeHandler();
       }
     }
   }
 };
+
+////////////////////////////////////////////////////////////////////////////////////////
 
 const headerView = {};
 
 const controlsView = {};
 
 const deckView = {
+
+  deck: document.querySelector('#deck'),
+
+  // will bind handlers to #deck...
+  bindClickHandler: function bindClickHandler() {
+    deckView.deck.addEventListener('click', controller.handleCardClick);
+  },
+
+  // will unbind click handler from #deck
+  unbindClickHandler: function unbindClickHandler() {
+    deckView.deck.removeEventListener('click', controller.handleCardClick);
+  },
+
+  // function to open a card...
   open: function open({ cardElement, cardName }) {
-    // adding cardName as a class to the i element
     cardElement.firstElementChild.classList.add(cardName);
   },
 
+  // function to close a card...
   close: function close({ cardElement, cardName }) {
     cardElement.firstElementChild.classList = "fa";
   },
